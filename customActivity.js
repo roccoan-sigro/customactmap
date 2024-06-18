9501716a8730cbf821313bfd18f080ee67f2d251
roccoan-sigro <105781020+roccoan-sigro@users.noreply.github.com>
2024-06-18 12:37:22 +0200

    Update customActivity.js

--- customActivity.js
+++ customActivity.js
@@ -1,4 +1,4 @@
-define('customActivity', ['postmonger'], function (Postmonger) {
+define('customActivity', ['jquery', 'postmonger'], function ($, Postmonger) {
     'use strict';
 
     var connection = new Postmonger.Session();
