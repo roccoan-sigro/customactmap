de6205b84912c411147f30f7d29d0af750fb20fa
roccoan-sigro <105781020+roccoan-sigro@users.noreply.github.com>
2024-06-20 11:43:35 +0200

    Update customActivity.js

--- customActivity.js
+++ customActivity.js
@@ -41,11 +41,10 @@ define('customActivity', ['jquery', 'postmonger'], function ($, Postmonger) {
             // Calcolare il centro e il raggio della selezione per ripristinare la mappa
             var centerLat = (parseFloat(coordinates.minLatitude) + parseFloat(coordinates.maxLatitude)) / 2;
             var centerLng = (parseFloat(coordinates.minLongitude) + parseFloat(coordinates.maxLongitude)) / 2;
-            var radius = calculateRadius(coordinates);
 
             // Inizializzare la mappa con la selezione salvata
             initializeMap(function (addMarkerAndCircle) {
-                addMarkerAndCircle([centerLat, centerLng], radius);
+                addMarkerAndCircle([centerLat, centerLng], 50); // Imposta il raggio iniziale a 50 metri
             });
         }
     }
@@ -86,17 +85,6 @@ define('customActivity', ['jquery', 'postmonger'], function ($, Postmonger) {
                 }
             });
         }
-    }
-
-    function calculateRadius(coords) {
-        var latDiff = parseFloat(coords.maxLatitude) - parseFloat(coords.minLatitude);
-        var lngDiff = parseFloat(coords.maxLongitude) - parseFloat(coords.minLongitude);
-        var avgLat = (parseFloat(coords.maxLatitude) + parseFloat(coords.minLatitude)) / 2;
-
-        var latRadius = (latDiff * 111139); // Convert degrees to meters (approx)
-        var lngRadius = (lngDiff * 111139 * Math.cos(avgLat * (Math.PI / 180))); // Adjust for latitude
-
-        return Math.max(latRadius, lngRadius) / 2; // Return the average radius
     }
 
     return {
