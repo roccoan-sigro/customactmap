f4d9d27438921624b23033998be46cf2fa34e905
roccoan-sigro <105781020+roccoan-sigro@users.noreply.github.com>
2024-06-20 11:24:52 +0200

    Update customActivity.js

--- customActivity.js
+++ customActivity.js
@@ -4,6 +4,7 @@ define('customActivity', ['jquery', 'postmonger'], function ($, Postmonger) {
     var connection = new Postmonger.Session();
     var payload = {};
     var eventDefinitionKey;
+    var coordinates = {};
 
     $(window).ready(onRender);
 
@@ -28,9 +29,25 @@ define('customActivity', ['jquery', 'postmonger'], function ($, Postmonger) {
             payload['arguments'].execute.inArguments.length > 0
         );
 
-        var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
+        if (hasInArguments) {
+            var inArguments = payload['arguments'].execute.inArguments[0];
+            coordinates = {
+                minLatitude: inArguments.minLatitude,
+                maxLatitude: inArguments.maxLatitude,
+                minLongitude: inArguments.minLongitude,
+                maxLongitude: inArguments.maxLongitude
+            };
 
-        // Initialize your form fields here with inArguments values, if necessary
+            // Calcolare il centro e il raggio della selezione per ripristinare la mappa
+            var centerLat = (parseFloat(coordinates.minLatitude) + parseFloat(coordinates.maxLatitude)) / 2;
+            var centerLng = (parseFloat(coordinates.minLongitude) + parseFloat(coordinates.maxLongitude)) / 2;
+            var radius = calculateRadius(coordinates);
+
+            // Inizializzare la mappa con la selezione salvata
+            initializeMap(function (addMarkerAndCircle) {
+                addMarkerAndCircle([centerLat, centerLng], radius);
+            });
+        }
     }
 
     function setInteraction(interaction) {
@@ -39,8 +56,7 @@ define('customActivity', ['jquery', 'postmonger'], function ($, Postmonger) {
         }
     }
 
-    function save(coordinates) {
-        // Use the selected coordinates from index.html
+    function save() {
         var minLatitude = coordinates.minLatitude;
         var maxLatitude = coordinates.maxLatitude;
         var minLongitude = coordinates.minLongitude;
@@ -58,8 +74,35 @@ define('customActivity', ['jquery', 'postmonger'], function ($, Postmonger) {
         connection.trigger('updateActivity', payload);
     }
 
+    function updateCoordinates(newCoordinates) {
+        coordinates = newCoordinates;
+    }
+
+    function initializeMap(callback) {
+        if (typeof callback === 'function') {
+            callback(function (latlng, radius) {
+                if (window.addMarkerAndCircle) {
+                    window.addMarkerAndCircle(latlng, radius);
+                }
+            });
+        }
+    }
+
+    function calculateRadius(coords) {
+        var latDiff = parseFloat(coords.maxLatitude) - parseFloat(coords.minLatitude);
+        var lngDiff = parseFloat(coords.maxLongitude) - parseFloat(coords.minLongitude);
+        var avgLat = (parseFloat(coords.maxLatitude) + parseFloat(coords.minLatitude)) / 2;
+
+        var latRadius = (latDiff * 111139); // Convert degrees to meters (approx)
+        var lngRadius = (lngDiff * 111139 * Math.cos(avgLat * (Math.PI / 180))); // Adjust for latitude
+
+        return Math.max(latRadius, lngRadius) / 2; // Return the average radius
+    }
+
     return {
         initialize: initialize,
-        save: save
+        save: save,
+        updateCoordinates: updateCoordinates,
+        initializeMap: initializeMap
     };
 });
