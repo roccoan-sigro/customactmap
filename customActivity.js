0ec6c172cb315a6e77937c6d1b9a0e3ca874cb39
roccoan-sigro <105781020+roccoan-sigro@users.noreply.github.com>
2024-07-01 17:55:34 +0200

    Update customActivity.js

--- customActivity.js
+++ customActivity.js
@@ -54,45 +54,38 @@ define('customActivity', ['jquery', 'postmonger'], function ($, Postmonger) {
     }
 
         function save() {
-        var minLatitude = coordinates.minLatitude;
-        var maxLatitude = coordinates.maxLatitude;
-        var minLongitude = coordinates.minLongitude;
-        var maxLongitude = coordinates.maxLongitude;
-        var userLatitude = coordinates.Latitudine || "{{Contact.Attribute.LongitudineLatitudine.Latitudine}}";
-        var userLongitude = coordinates.Longitudine || "{{Contact.Attribute.LongitudineLatitudine.Longitudine}}";
-    
-        payload['arguments'].execute.inArguments = [{
-            "minLatitude": minLatitude,
-            "maxLatitude": maxLatitude,
-            "minLongitude": minLongitude,
-            "maxLongitude": maxLongitude,
-            "Latitudine": userLatitude,
-            "Longitudine": userLongitude,
-            "SubscriberKey": "{{Contact.Key}}",
-            "EmailAddress": "{{InteractionDefaults.Email}}"
-        }];
-    
-        payload['metaData'].isConfigured = true;
-    
-        console.log('Saving payload:', JSON.stringify(payload, null, 2));
-        connection.trigger('updateActivity', payload);
-    }
-
-
-    function updateCoordinates(newCoordinates) {
-        console.log('New coordinates updated:', newCoordinates);
-        coordinates = newCoordinates;
-    }
-
-    function initializeMap(callback) {
-        if (typeof callback === 'function') {
-            callback(function (latlng, radius) {
-                if (window.addMarkerAndCircle) {
-                    window.addMarkerAndCircle(latlng, radius);
-                }
-            });
+            var minLatitude = coordinates.minLatitude;
+            var maxLatitude = coordinates.maxLatitude;
+            var minLongitude = coordinates.minLongitude;
+            var maxLongitude = coordinates.maxLongitude;
+            var userLatitude = coordinates.Latitudine || "{{Contact.Attribute.LongitudineLatitudine.Latitudine}}";
+            var userLongitude = coordinates.Longitudine || "{{Contact.Attribute.LongitudineLatitudine.Longitudine}}";
+        
+            payload['arguments'].execute.inArguments = [{
+                "minLatitude": minLatitude,
+                "maxLatitude": maxLatitude,
+                "minLongitude": minLongitude,
+                "maxLongitude": maxLongitude,
+                "Latitudine": userLatitude,
+                "Longitudine": userLongitude,
+                "SubscriberKey": "{{Contact.Key}}",
+                "EmailAddress": "{{InteractionDefaults.Email}}"
+            }];
+        
+            payload['metaData'].isConfigured = true;
+        
+            // Aggiorna il nome del primo outcome con i dettagli dell'indirizzo
+            var address = document.getElementById('address').innerText.replace('Indirizzo: ', '');
+            var radius = document.getElementById('radiusInput').value;
+            var outcomeLabel = `${address}, ${radius}m`;
+        
+            // Aggiorna il nome del primo outcome nel config.json
+            payload['outcomes'][0].metaData.label = outcomeLabel;
+        
+            console.log('Saving payload:', JSON.stringify(payload, null, 2));
+            connection.trigger('updateActivity', payload);
         }
-    }
+
 
     return {
         initialize: initialize,
